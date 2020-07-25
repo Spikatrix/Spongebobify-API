88066d2220fadc9b251bae7418ebe73019f150a2
Spikatrix <12792882+Spikatrix@users.noreply.github.com>
2020-07-25 18:06:37 +0530

    Return JSON, Query supported too

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const API_URL = '/api/v1/spongebobify';
 
 app.get('/', (req, res) => {
-	res.status(400).send(`Send a string to ${API_URL}`)
+	res.status(400).send(`Send a GET request or a query with a string in the 'text' key parameter to ${API_URL} to spongebobify the text`)
 });
 
-app.post(API_URL, (req, res) => {
+app.get(API_URL, (req, res) => {
 	var success;
 	var message;
-	if(req.body.text) {
+	if (req.query.text) {
+		success = 'true';
+		message = spongebobifyText(req.query.text);
+	} else if (req.body.text) {
 		success = 'true';
 		message = spongebobifyText(req.body.text);
 	} else {
@@ -22,14 +25,7 @@ app.post(API_URL, (req, res) => {
 		message = '\'text\' parameter not found';
 	}
 
-	var statusCode;
-	if (success == 'true') {
-		statusCode = 200;
-	} else {
-		statusCode = 400;
-	}
-
-	res.status(statusCode).send({
+	res.json({
 		success: success,
 		message: message,
 	})
@@ -44,7 +40,7 @@ app.listen(PORT, () => {
 
 function spongebobifyText(str) {
 	let mockStr = ""
-	for(let i = 0; i < str.length; i++) {
+	for (let i = 0; i < str.length; i++) {
 		if (Math.floor(Math.random() * 2) == 0) {
 			mockStr += str.charAt(i).toLowerCase()
 		} else {
