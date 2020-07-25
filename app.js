57c56362b694a261aff22e9287b6f4ce2700f3ab
Spikatrix <12792882+Spikatrix@users.noreply.github.com>
2020-07-25 15:43:44 +0530

    Herokufication 1
    Port and engine

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.post(API_URL, (req, res) => {
 	})
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`)
