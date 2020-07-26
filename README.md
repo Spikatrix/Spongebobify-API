7ad0171f2a514553bd6079a4a3cdfb21dae698ed
Spikatrix <12792882+Spikatrix@users.noreply.github.com>
2020-07-26 11:53:38 +0530

    Herokufication and other improvements (merge deployment)

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Spongebobify Text
 
 Converts text like this to teXT lIkE tHIs
+
+Hosted on [Heroku](http://nameless-waters-90998.herokuapp.com/)
--- app.js
+++ app.js
@@ -8,13 +8,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const API_URL = '/api/v1/spongebobify';
 
 app.get('/', (req, res) => {
-	res.status(400).send(`Send a string to ${API_URL}`)
+	var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl.substring(0, req.originalUrl.length - 1);
+	res.status(400).send(`Send a GET request or a query with a string in the '<i>text</i>' key parameter to ` + 
+						 `<b>${fullUrl}${API_URL}</b> to spongebobify the text<br><br>` + 
+						 `Example: ${fullUrl}${API_URL}?text=Spongebobify`)
 });
 
-app.post(API_URL, (req, res) => {
+app.get(API_URL, (req, res) => {
 	var success;
 	var message;
-	if(req.body.text) {
+	if (req.query.text) {
+		success = 'true';
+		message = spongebobifyText(req.query.text);
+	} else if (req.body.text) {
 		success = 'true';
 		message = spongebobifyText(req.body.text);
 	} else {
@@ -22,20 +28,13 @@ app.post(API_URL, (req, res) => {
 		message = '\'text\' parameter not found';
 	}
 
-	var statusCode;
-	if (success == 'true') {
-		statusCode = 200;
-	} else {
-		statusCode = 400;
-	}
-
-	res.status(statusCode).send({
+	res.json({
 		success: success,
 		message: message,
 	})
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`)
@@ -44,7 +43,7 @@ app.listen(PORT, () => {
 
 function spongebobifyText(str) {
 	let mockStr = ""
-	for(let i = 0; i < str.length; i++) {
+	for (let i = 0; i < str.length; i++) {
 		if (Math.floor(Math.random() * 2) == 0) {
 			mockStr += str.charAt(i).toLowerCase()
 		} else {
